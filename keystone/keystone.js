d2a213a118596dd2af2a12a5d7b9c39b37ac9677
hugo <hugo.mineiro.r@gmail.com>
2017-05-03 16:30:13 +0100

    adding keystone files

---
+++ keystone/keystone.js
@@ -0,0 +1,50 @@
+// Simulate config options from your production environment by
+// customising the .env file in your project's root folder.
+require('dotenv').config();
+
+// Require keystone
+var keystone = require('keystone');
+var handlebars = require('express-handlebars');
+
+keystone.init({
+	'name': 'website',
+	'brand': 'website',
+
+	'sass': 'public',
+	'static': 'public',
+	'favicon': 'public/favicon.ico',
+	'views': 'templates/views',
+	'view engine': '.hbs',
+
+	'custom engine': handlebars.create({
+		layoutsDir: 'templates/views/layouts',
+		partialsDir: 'templates/views/partials',
+		defaultLayout: 'default',
+		helpers: new require('./templates/views/helpers')(),
+		extname: '.hbs',
+	}).engine,
+
+	'auto update': true,
+	'session': true,
+	'auth': true,
+	'user model': 'User',
+});
+keystone.import('models');
+keystone.set('locals', {
+	_: require('lodash'),
+	env: keystone.get('env'),
+	utils: keystone.utils,
+	editable: keystone.content.editable,
+});
+keystone.set('routes', require('./routes'));
+
+keystone.set('nav', {
+	posts: ['posts', 'post-categories'],
+	galleries: 'galleries',
+	enquiries: 'enquiries',
+	users: 'users',
+});
+
+
+
+keystone.start();
